92187da0e9f31ee7efc14cd62718339ef6d92b21
taniadruzh <tania.druzhchenko@gmail.com>
2018-01-06 23:25:02 +0200

    string

--- src/Main.java
+++ src/Main.java
@@ -1,105 +1,76 @@
+import java.util.Scanner;
+
 /**
- * Created by java on 26.12.2017.
+ * Created by java on 29.12.2017.
  */
 public class Main {
     public static void main(String[] args) {
-//        int a = 15;
-//        long b = 12;
-//        System.out.println(a);
-//        System.out.println(b);
-//        float f = 22.5f;
-//        double d = 33.3d;
-//        long ab = 12;
-//        helloLong(ab);
-        //a+b
-//        System.out.println(12 + 15);
-//        int a= 12;
-//        int b = 15;
-//        System.out.println(a + b);
-//        int sum = a + b;
-//        System.out.println(sum);
-//        int a = 15;
-//        long b = 15;
-//        int sum =( int)( b + a);
-//        int o = (int) (a % b);
-//        double d = 22.5d;
-//        float f = 55.2f;
-//        float ff = (float) (f * d);
-//        boolean flag1 = false;
-//        boolean flag2 = true;
-//        System.out.println(flag1);
-//        System.out.println(flag2);
-//        String str = "stroka";
-//        System.out.println(str);
-//        System.out.println("dlina stroki " + str.length());
-//        System.out.println(str.contains("t"));
-//        String str2 = "stroka";
-//        System.out.println(str == str2);
-/////////////////////////////
-//        String strNew = new String("fox");
-//        String strNew1 = new String("fox");
-//        System.out.println(strNew == strNew1);
-//////////////////////
-//        String str = "1";
-//        String str1 = "2";
-//        System.out.println(Integer.parseInt(str) + Integer.parseInt(str1));
-//        System.out.println(Float.parseFloat(str) + Float.parseFloat(str1));
-//        System.out.println(Double.parseDouble(str) + Double.parseDouble(str1));
-//        System.out.println(Float.parseFloat(str) + Float.parseFloat(str1));
-///////////////////
-//        System.out.println((int) str + (int) str1);
-//        int a = 15;
-//        String aStr = String.valueOf(a);
-//        System.out.println(aStr);
-//
-//        int b = Integer.parseInt(aStr);
-//        float flt = Float.parseFloat(aStr);
-///////////////
-//        String str = "www34";
-//        String str3 = str.substring(3,5);
-//        System.out.println(Integer.parseInt(str3));
-////////////////////////////
-//        int mass[] = new int[12];
-//        int [] mass2  = {1,2,3,4,5};
-//        System.out.println(mass.length);
-////////////////////
-//        int mass [] = new int[10];
-//
-//        mass[0] = 10;
-//        mass[5] = 15;
-//        mass[7] = 20;
-//
-//        for (int mas : mass) {
-//            System.out.println(mas);
+//        String str = "Был и я среди донцов...\n"+
+//        "Была пора: наш праздник молодой...\n"+
+//        "В альбом (Гонимый рока самовластьем...)\n"+
+//        "В альбом (Долго сих листов заветных...)\n"+
+//        "В альбом А. О. Смирновой\n"+
+//        "В альбом княжны А. Д. Абамалек\n"+
+//        "В альбом Павлу Вяземскому\n"+
+//        "В еврейской хижине лампада...\n"+
+//        "В крови горит огонь желанья...\n"+
+//        "В мои осенние досуги...\n"+
+//        "В начале жизни школу помню я...\n"+
+//        "В поле чистом серебрится Снег...\n";
+//        System.out.println(str.replace("В",":-)"));
+//        String str1 = "Был и я среди донцов...\n"+
+//                "Была пора: наш праздник молодой...\n"+
+//                "В альбом (Гонимый рока самовластьем...)\n"+
+//                "В альбом (Долго сих листов заветных...)\n"+
+//                "В альбом А. О. Смирновой\n"+
+//                "В альбом княжны А. Д. Абамалек\n"+
+//                "В альбом Павлу Вяземскому\n"+
+//                "В еврейской хижине лампада...\n"+
+//                "В крови горит огонь желанья...\n"+
+//                "В мои осенние досуги...\n"+
+//                "В начале жизни школу помню я...\n"+
+//                "В поле чистом серебрится Снег...\n";
+//        char ch1[] = str1.toCharArray();
+//        for (int i = 0; i <= ch1.length; i++) {
+//            if (i%2 == 0  ) System.out.print(ch1[i]);
 //        }
-//
-//        for (int i = 0; i < mass.length; i++)
-//        {
-//            System.out.print(mass[i] + "  ");
+//        String str2 = "Был и я среди донцов...\n"+
+//                "Была пора: наш праздник молодой...\n"+
+//                "В альбом (Гонимый рока самовластьем...)\n"+
+//                "В альбом (Долго сих листов заветных...)\n"+
+//                "В альбом А. О. Смирновой\n"+
+//                "В альбом княжны А. Д. Абамалек\n"+
+//                "В альбом Павлу Вяземскому\n"+
+//                "В еврейской хижине лампада...\n"+
+//                "В крови горит огонь желанья...\n"+
+//                "В мои осенние досуги...\n"+
+//                "В начале жизни школу помню я...\n"+
+//                "В поле чистом серебрится Снег...\n";
+//        String [] strMass = str2.split(" ");
+//        int num = 0;
+//        String alb = "альбом";
+//        for (String strMas : strMass) {
+//            if (strMas.equals(alb)) num ++;
 //        }
-        String mass [] = {"one","two","five"};
+//        System.out.println(str1);
+//        System.out.println(num);
 
+        for (int i = 0; i <= 10; i++)
+        {
+            System.out.println("Type any number - ");
+            Scanner in = new Scanner(System.in);
+            int n = in.nextInt();
+            int o = n % 2;
+            switch (o) {
+                case 0:
+                    System.out.println("Четное число");
+                    break;
+                default:
+                    System.out.println("Нечетное число");
 
-        for (String  mas : mass) {
-            System.out.println(mas);
+            }
         }
 
-///////////////////
-        //HomeWork
-//        String arr = "asdfghjkl";
-//        String mStr [] = new String [arr.length()];
-//
-//        for (int i = 0; i < mStr.length; i ++) {
-//            mStr[i] = arr.substring(i,i+1);
-//        }
-//        for (String s : mStr) {
-//            System.out.println(s);
-//        }
-/////////////////
-    }
-    public static void helloLong(long l)
-    {
-        System.out.println(l);
     }
 
 }
