79939d0f33ac20944bb8cded3f683464a162c66b
taniadruzh <tania.druzhchenko@gmail.com>
2018-01-06 23:14:37 +0200

    ht1

--- src/Main.java
+++ src/Main.java
@@ -1,89 +1,105 @@
+/**
+ * Created by java on 26.12.2017.
+ */
 public class Main {
     public static void main(String[] args) {
-        /////////////// 1
-        System.out.println("1.");
-        int n = 6;
-        float f = n % 2;
-        if (f == 0)
-            System.out.println("Even number");
-        else
-            System.out.println("Odd number");
-        //////////// 2
-        System.out.println("2.");
-        double n2 = 13.3d;
-        double m2 = 8.7d;
-        if (Math.abs(10 - n2) < Math.abs(10 - m2))
-            System.out.println(n2);
-        else
-            System.out.println(m2);
-        //////////// 3
-        System.out.println("3.");
-        for (int i = -10; i<=25; i ++)
-        {
-            System.out.print(i+ " ");
-        }
-        System.out.println();
-        int i = -10;
-        while (i <= 25)
-        {
-            System.out.print(i + " ");
-            i++;
-        }
-        System.out.println();
-        i = -10;
-        do {
-            System.out.print(i + " ");
-            i ++;
-        }while (i <= 25);
-        //////////////// 4
-        System.out.println("4.");
-        for (int j = 1; j<=100; j ++)
-        {
-            if (j % 5 == 0) System.out.print(j + " ");
-        }
-        System.out.println();
-        ////////////// 5
-        System.out.println("5.");
-        int arr[] = {5,4,3,2,1,4,7,8,5,9,5,2,3};
-        int min = arr[0];
-        int max = arr[0];
-        for (int i1 : arr) {
-            if (i1 < min)
-            {
-                min = i1;
-            }
-            if (i1 > max)
-            {
-                max = i1;
-            }
-        }
-        System.out.println("Min = " + min);
-        System.out.println("Max = " + max);
-        int sum = 0;
-        for (int i1 : arr) {
-            if (i1 != max || i1 != min) sum = sum + i1;
-        }
-        System.out.println("Sum = " + sum);
-        ///////////// 6
-        System.out.println("6.");
-        String arr6 = "asdfghjkl";
-        String mStr [] = new String [arr6.length()];
+//        int a = 15;
+//        long b = 12;
+//        System.out.println(a);
+//        System.out.println(b);
+//        float f = 22.5f;
+//        double d = 33.3d;
+//        long ab = 12;
+//        helloLong(ab);
+        //a+b
+//        System.out.println(12 + 15);
+//        int a= 12;
+//        int b = 15;
+//        System.out.println(a + b);
+//        int sum = a + b;
+//        System.out.println(sum);
+//        int a = 15;
+//        long b = 15;
+//        int sum =( int)( b + a);
+//        int o = (int) (a % b);
+//        double d = 22.5d;
+//        float f = 55.2f;
+//        float ff = (float) (f * d);
+//        boolean flag1 = false;
+//        boolean flag2 = true;
+//        System.out.println(flag1);
+//        System.out.println(flag2);
+//        String str = "stroka";
+//        System.out.println(str);
+//        System.out.println("dlina stroki " + str.length());
+//        System.out.println(str.contains("t"));
+//        String str2 = "stroka";
+//        System.out.println(str == str2);
+/////////////////////////////
+//        String strNew = new String("fox");
+//        String strNew1 = new String("fox");
+//        System.out.println(strNew == strNew1);
+//////////////////////
+//        String str = "1";
+//        String str1 = "2";
+//        System.out.println(Integer.parseInt(str) + Integer.parseInt(str1));
+//        System.out.println(Float.parseFloat(str) + Float.parseFloat(str1));
+//        System.out.println(Double.parseDouble(str) + Double.parseDouble(str1));
+//        System.out.println(Float.parseFloat(str) + Float.parseFloat(str1));
+///////////////////
+//        System.out.println((int) str + (int) str1);
+//        int a = 15;
+//        String aStr = String.valueOf(a);
+//        System.out.println(aStr);
+//
+//        int b = Integer.parseInt(aStr);
+//        float flt = Float.parseFloat(aStr);
+///////////////
+//        String str = "www34";
+//        String str3 = str.substring(3,5);
+//        System.out.println(Integer.parseInt(str3));
+////////////////////////////
+//        int mass[] = new int[12];
+//        int [] mass2  = {1,2,3,4,5};
+//        System.out.println(mass.length);
+////////////////////
+//        int mass [] = new int[10];
+//
+//        mass[0] = 10;
+//        mass[5] = 15;
+//        mass[7] = 20;
+//
+//        for (int mas : mass) {
+//            System.out.println(mas);
+//        }
+//
+//        for (int i = 0; i < mass.length; i++)
+//        {
+//            System.out.print(mass[i] + "  ");
+//        }
+        String mass [] = {"one","two","five"};
 
-        for (int i6 = 0; i6 < mStr.length; i6 ++) {
-            mStr[i6] = arr6.substring(i6,i6+1);
-        }
-        for (String s : mStr) {
-            System.out.println(s);
-        }
-        ///////////////
-        short sh [] = new short[100];
-        int in = 0;
-        for (short ii = 100; ii>0; ii--)
-        {
-            sh[in] = ii;
-            System.out.print(sh[in] + " ");
-            in ++;
+
+        for (String  mas : mass) {
+            System.out.println(mas);
         }
 
+///////////////////
+        //HomeWork
+//        String arr = "asdfghjkl";
+//        String mStr [] = new String [arr.length()];
+//
+//        for (int i = 0; i < mStr.length; i ++) {
+//            mStr[i] = arr.substring(i,i+1);
+//        }
+//        for (String s : mStr) {
+//            System.out.println(s);
+//        }
+/////////////////
     }
+    public static void helloLong(long l)
+    {
+        System.out.println(l);
+    }
+
 }
